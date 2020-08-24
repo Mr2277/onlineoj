6ebca06dbef8b89358f46830246f906ad85639c8
sun <837544713@qq.com>
2020-08-24 23:02:40 +0800

    add leetcode4

---
+++ src/main/java/com/huawei/leetcode/LeetCode4.java
@@ -0,0 +1,9 @@
+package com.huawei.leetcode;
+
+import java.util.Scanner;
+
+public class LeetCode4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
