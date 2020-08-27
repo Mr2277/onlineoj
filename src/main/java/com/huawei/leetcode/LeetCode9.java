7e0415e07f01cd3566805f907e43a46788deb2f7
sun <837544713@qq.com>
2020-08-27 20:33:51 +0800

    add leetcode9

---
+++ src/main/java/com/huawei/leetcode/LeetCode9.java
@@ -0,0 +1,28 @@
+package com.huawei.leetcode;
+
+import java.util.Scanner;
+
+public class LeetCode9 {
+    public static boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        int cur = 0;
+        int num = x;
+        while(num != 0) {
+            cur = cur * 10 + num % 10;
+            num /= 10;
+        }
+        return cur == x;
+
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int num = scanner.nextInt();
+            isPalindrome(num);
+        }
+        
+
+    }
+}
