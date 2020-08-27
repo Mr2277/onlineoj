34ac9017ba4fc64d7f5aac9aa27069291b7e814a
sun <837544713@qq.com>
2020-08-27 22:18:54 +0800

    leetcode10

---
+++ src/main/java/com/huawei/leetcode/LeetCode10.java
@@ -0,0 +1,15 @@
+package com.huawei.leetcode;
+
+import java.util.Scanner;
+
+public class LeetCode10 {
+    public static boolean isMatch(String s, String p) {
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
