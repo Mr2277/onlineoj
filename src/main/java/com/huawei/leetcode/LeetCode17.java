e3506e1ff08c07fcf1a841fb389f899a446f34fd
sun <837544713@qq.com>
2020-09-03 22:59:04 +0800

    leetcode17

--- src/main/java/com/huawei/leetcode/LeetCode17.java
+++ src/main/java/com/huawei/leetcode/LeetCode17.java
@@ -4,22 +4,6 @@ import java.util.*;
 
 public class LeetCode17 {
     public static List<String> letterCombinations(String digits) {
-        String num = String.valueOf(digits.charAt(0));
-        List<String> result = new ArrayList<String>();
-        StringBuilder stringBuilder = new StringBuilder();
-        DFS(num, digits, 0, 1, result, stringBuilder);
-        return null;
-    }
-    public static List<String> DFS(String num, String digits, int level, int index, List<String> result, StringBuilder stringBuilder) {
-        String indexMapString = getNumMap().get(index);
-        if (level == digits.length() && index > indexMapString.length() - 1) {
-            result.add(stringBuilder.toString());
-        } else {
-            String curStr = String.valueOf(indexMapString.charAt(index));
-        }
-        return result;
-    }
-    public static Map<String, String> getNumMap() {
         Map<String, String> numMap = new HashMap<String, String>();
         numMap.put("2", "abc");
         numMap.put("3", "def");
@@ -29,8 +13,31 @@ public class LeetCode17 {
         numMap.put("7", "pqrs");
         numMap.put("8", "tuv");
         numMap.put("9", "wxyz");
-        return numMap;
+        List<String> result = new ArrayList<String>();
+        StringBuilder builder = new StringBuilder();
+        DFS(digits, numMap, 0, result, builder);
+        return null;
     }
+    public static List<String> DFS(String digits, Map<String, String> numMap, int level, List<String> result, StringBuilder builder) {
+        String curNum = String.valueOf(digits.charAt(level));
+        String curStr = numMap.get(curNum);
+        for (int i = 0; i < curStr.length(); i++) {
+            String curChar = String.valueOf(curStr.charAt(i));
+            if (level < digits.length() - 1) {
+                builder.append(curChar);
+                result = DFS(digits, numMap, level + 1, result, builder);
+                builder.deleteCharAt(level);
+            } else {
+                builder.append(curChar);
+                if (builder.length() == digits.length()) {
+                    result.add(builder.toString());
+                }
+                builder.deleteCharAt(level);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
