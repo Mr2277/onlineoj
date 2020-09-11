c952b871a43ac5b2207e917863ec57a66f8c6e9d
sun <837544713@qq.com>
2020-09-11 22:31:10 +0800

    add LeeetCode25

--- src/main/java/com/huawei/leetcode/Leetcode25.java
+++ src/main/java/com/huawei/leetcode/Leetcode25.java
@@ -1,4 +1,51 @@
 package com.huawei.leetcode;
 
+import java.util.Scanner;
+
 public class Leetcode25 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode tail = head;
+        for (int i = 0; i < k; i++) {
+            if (tail == null) {
+                return head;
+            }
+            tail = tail.next;
+        }
+        ListNode newHead = reverse(head, tail);
+        head.next = reverseKGroup(tail, k);
+        return newHead;
+    }
+
+    public ListNode reverse(ListNode head, ListNode tail) {
+        ListNode pre = null, current = head, next = head;
+        while (current != tail) {
+            next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            ListNode head = new ListNode(Integer.MAX_VALUE);
+            ListNode temp = head;
+            for (int i = 0; i < n; i++) {
+                int val = scanner.nextInt();
+                ListNode node = new ListNode(val);
+                node.next = null;
+                temp.next = node;
+                temp = node;
+            }
+            // StringBuilder builder = new StringBuilder();
+        }
+    }
 }
