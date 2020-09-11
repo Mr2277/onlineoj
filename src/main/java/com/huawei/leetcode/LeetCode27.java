df8d925186f99aa328856d69abe70980ace5fd52
sun <837544713@qq.com>
2020-09-12 06:57:56 +0800

    add leetcode27

---
+++ src/main/java/com/huawei/leetcode/LeetCode27.java
@@ -0,0 +1,33 @@
+package com.huawei.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LeetCode27 {
+
+    public static int removeElement(int[] nums, int val) {
+        int length = nums.length, ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                length --;
+            } else {
+                nums[ans++] = nums[i];
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = scanner.nextInt();
+            }
+            int val = scanner.nextInt();
+            removeElement(nums, val);
+        }
+    }
+}
