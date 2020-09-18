ba1474ea53fe64e177c8405e24ea4a1a6bbe6130
sun <837544713@qq.com>
2020-09-18 08:01:48 +0800

    leetcode32

--- src/main/java/com/huawei/leetcode/leetcode31_40/LeetCode32.java
+++ src/main/java/com/huawei/leetcode/leetcode31_40/LeetCode32.java
@@ -1,24 +1,25 @@
 package com.huawei.leetcode.leetcode31_40;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class LeetCode32 {
     public static int longestValidParentheses(String s) {
-        Stack<String> stack = new Stack();
-        boolean lastMatch = false;
+        Stack<Map<Integer, String>> stack = new Stack();
+        int max = 0;
         for (int i = 0; i < s.length(); i++) {
             String curcent = String.valueOf(s.charAt(i));
             if (curcent.equals("(")) {
-                stack.add(curcent);
+                Map<Integer, String> map = new HashMap<>();
+                map.put(i, curcent);
             } else if (!stack.isEmpty()) {
-                String stackTop = stack.peek();
-                if (stackTop.equals("(")) {
+                Map<Integer, String> stackTop = stack.peek();
+                Iterator iterator = stackTop.entrySet().iterator();
+                while (iterator.hasNext()) {
 
                 }
             }
         }
-        return 0;
+        return max;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
