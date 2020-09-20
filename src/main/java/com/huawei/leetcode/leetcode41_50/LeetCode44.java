c0b06cff1e50f6b85d48143277ba2e6846b2c0f6
sun <837544713@qq.com>
2020-09-20 23:04:55 +0800

    add LeetCode44

--- src/main/java/com/huawei/leetcode/leetcode41_50/LeetCode44.java
+++ src/main/java/com/huawei/leetcode/leetcode41_50/LeetCode44.java
@@ -3,7 +3,7 @@ package com.huawei.leetcode.leetcode41_50;
 import java.util.*;
 
 public class LeetCode44 {
-
+    /*
     public static int min = Integer.MAX_VALUE;
 
     public static int jump(int[] nums) {
@@ -24,6 +24,23 @@ public class LeetCode44 {
             DFS(nums, i, skipTimes + 1);
         }
     }
+    */
+
+    public static int jump(int[] nums) {
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            //找能跳的最远的
+            maxPosition = Math.max(maxPosition, nums[i] + i);
+            if( i == end){ //遇到边界，就更新边界，并且步数加一
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -32,7 +49,7 @@ public class LeetCode44 {
             for (int i = 0; i < n; i++) {
                 nums[i] = scanner.nextInt();
             }
-            jump(nums);
+            System.out.println(jump(nums));
         }
     }
 }
