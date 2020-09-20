6903dfd2126d7e05044e2ba294e8c05114439189
sun <837544713@qq.com>
2020-09-20 16:35:10 +0800

    add LeetCode43

---
+++ src/main/java/com/huawei/leetcode/leetcode41_50/LeetCode43.java
@@ -0,0 +1,22 @@
+package com.huawei.leetcode.leetcode41_50;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LeetCode43 {
+    public static String multiply(String num1, String num2) {
+        BigInteger integer1 = new BigInteger(num1);
+        BigInteger integer2 = new BigInteger(num2);
+        BigInteger integer3 = integer1.multiply(integer2);
+        String result = integer3.toString();
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String num1 = scanner.nextLine();
+            String num2 = scanner.nextLine();
+            multiply(num1, num2);
+        }
+    }
+}
