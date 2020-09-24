034ece1891252dc4a380159a9ac9f5a955a7dab9
sun <837544713@qq.com>
2020-09-24 21:41:05 +0800

    add LeetCode49

--- src/main/java/com/huawei/leetcode/leetcode41_50/LeetCode48.java
+++ src/main/java/com/huawei/leetcode/leetcode41_50/LeetCode48.java
@@ -3,33 +3,38 @@ package com.huawei.leetcode.leetcode41_50;
 import java.util.Scanner;
 
 public class LeetCode48 {
+    /*
     public static void rotate(int[][] matrix) {
-        //System.out.println(matrix.length);
-        int rounds = matrix.length / 2, count = 0, flag = 0, i = 0;
-        int[] temp0 = new int[matrix.length];
-        int[] temp1 = new int[matrix.length];
-        int[] temp2 = new int[matrix.length];
-        int[] temp3 = new int[matrix.length];
-        int length = matrix.length;
-        while (count < rounds) {
-            if (flag == 0) {
-                temp0[i] = matrix[i][length - 1 - count];
-                matrix[i][length - 1 - count] = matrix[count][i];
-                i++;
-                if (i == length - count) {
-                    flag = 1;
-                    i = count;
-                }
-            } else if (flag == 1) {
-                temp1[i] = matrix[length - 1 - count][length - 1 - i];
-                matrix[length - 1 - count][length - 1 - i] = temp0[i];
-                i++;
-                if (i == length - count) {
-                    flag = 2;
-                    i = count;
-                }
+        int add = 0;
+        int temp = 0;
+        int pos1 = 0;
+        int pos2 = matrix[0].length - 1;
+        while (pos1 < pos2) {
+            add = 0;
+            while (add < pos2 - pos1) {
+                temp = matrix[pos1][pos1 + add];
+                matrix[pos1][pos1 + add] = matrix[pos2 - add][pos1];
+                matrix[pos2 - add][pos1] = matrix[pos2][pos2 - add];
+                matrix[pos2][pos2 - add] = matrix[pos1 + add][pos2];
+                matrix[pos1 + add][pos2] = temp;
+                add++;
+            }
+            pos1++;
+            pos2--;
+
+        }
+    }
+    */
+
+    public static void rotate(int[][] matrix) {
+        int[][] newMatrix = new int[matrix.length][matrix.length];
+        int i = 0, j = 0, length = matrix.length - 1;
+        for (i = 0; i < matrix.length; i++) {
+            for(j = 0; j < matrix.length; j++) {
+                newMatrix[i][j] = matrix[length - j][i];
             }
         }
+        matrix = newMatrix;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
