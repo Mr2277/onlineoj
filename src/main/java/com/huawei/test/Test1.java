9af7209ca1b5d5ba62e68d8e440433b83fb8cd8e
sun <837544713@qq.com>
2020-09-26 13:27:12 +0800

    add LeetCode110

--- src/main/java/com/huawei/test/Test1.java
+++ src/main/java/com/huawei/test/Test1.java
@@ -3,6 +3,7 @@ package com.huawei.test;
 // git log --author=sun --since=2020-08-01 --until=2020-09-01 --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --author="$name" --pretty=tformat: --numstat | grep "\(.html\|.java\|.xml\|.properties\)$" | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -; done
 
 
+import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
@@ -12,8 +13,19 @@ public class Test1 {
         num = ch;
         System.out.println(num);
         */
+        /*
         String str = "192.168.192.0";
         String[] strings = str.split("\\.");
         System.out.println(strings.length);
+        */
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            if ((n & 1) == 1) {
+                System.out.println("qi");
+            } else {
+                System.out.println("ou");
+            }
+        }
     }
 }
