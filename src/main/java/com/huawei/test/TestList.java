5d01632d550deb237e5cafdc18727c7c62198650
sun <837544713@qq.com>
2020-09-30 07:53:45 +0800

    add TestList

--- src/main/java/com/huawei/test/TestList.java
+++ src/main/java/com/huawei/test/TestList.java
@@ -1,12 +1,25 @@
 package com.huawei.test;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TestList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-
+            List<String> list = new ArrayList<>();
+            Set<String> set = new HashSet<String>();
+            list.add("str");
+            list.add("ddd");
+            set.add("str");
+            List<String> update = list.stream().filter(a -> set.contains(a)).collect(Collectors.toList());
+            List<String> insert = list.stream().filter(a -> !set.contains(a)).collect(Collectors.toList());
+            for (String s : update) {
+                System.out.println(s);
+            }
+            for (String s : insert) {
+                System.out.println(s);
+            }
         }
     }
 }
