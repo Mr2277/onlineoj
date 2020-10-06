709e3e5a3a43572ba367ddfc0ae8439325affcb1
sun <837544713@qq.com>
2020-10-06 15:28:59 +0800

    LeetCode111

--- src/main/java/com/huawei/leetcode/leetcode111_120/LeetCode111.java
+++ src/main/java/com/huawei/leetcode/leetcode111_120/LeetCode111.java
@@ -1,8 +1,123 @@
 package com.huawei.leetcode.leetcode111_120;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class LeetCode111 {
 
-    public static void main(String[] args) {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 
+    public static TreeNode create(String str) {
+        int index = 0;
+        char ch = str.charAt(index);
+        String cur = "", pre = "", next = "";
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode parent = null, node = null;
+        while (index < str.length()) {
+            while (ch >= '0' && ch <= '9' || ch == '#') {
+                cur += ch;
+                index++;
+                ch = str.charAt(index);
+            }
+            next = String.valueOf(ch);
+            if (!"".equals(cur) && !cur.equals("#")) {
+                Integer curVal = Integer.valueOf(cur);
+                node = new TreeNode(curVal);
+                node.left = null;
+                node.right = null;
+            }
+            switch (pre) {
+
+                case "(":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.left = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ",":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.right = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ")":
+                    treeNodeStack.pop();
+                    break;
+                default:
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+            }
+
+            pre = "".equals(cur) ? String.valueOf(str.charAt(index)) : next;
+            cur = "";
+            index++;
+            ch = index < str.length() ? str.charAt(index) : ')';
+        }
+        return treeNodeStack.peek();
+    }
+    /*
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null || root.right != null) {
+            return 1;
+        } else {
+            int result = DFS(root, 1);
+            // System.out.println(result);
+            return result;
+        }
+    }
+    */
+    public static int DFS(TreeNode node, int depth) {
+        if (node == null) {
+            return depth - 1;
+        } else {
+            int depthLeft = DFS(node.left, depth + 1);
+            int depthRight = DFS(node.right, depth + 1);
+            return Integer.min(depthLeft, depthRight);
+        }
+    }
+
+    public static int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.left == null && root.right == null) {
+            return 1;
+        }
+        int ans = Integer.MAX_VALUE;
+        if(root.left != null) {
+            ans = Math.min(minDepth(root.left), ans);
+        }
+        if(root.right != null) {
+            ans = Math.min(minDepth(root.right), ans);
+        }
+        return ans + 1;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            TreeNode root = create(str);
+            minDepth(root);
+        }
     }
 }
