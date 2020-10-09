215043906ab544fd1c62bb9ddeff4e9f56401cd9
sun <837544713@qq.com>
2020-10-09 23:35:06 +0800

    add test Map remove null

--- src/main/java/com/huawei/test/Test1.java
+++ src/main/java/com/huawei/test/Test1.java
@@ -3,6 +3,8 @@ package com.huawei.test;
 //git log --author=sun --since=2020-08-01 --until=2020-09-01 --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --author="$name" --pretty=tformat: --numstat | grep "\(.html\|.java\|.xml\|.properties\)$" | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -; done
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Test1 {
@@ -18,6 +20,7 @@ public class Test1 {
         String[] strings = str.split("\\.");
         System.out.println(strings.length);
         */
+        /*
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             int n = scanner.nextInt();
@@ -27,5 +30,10 @@ public class Test1 {
                 System.out.println("ou");
             }
         }
+        */
+        Map<String, String> map = new HashMap<>();
+        map.put("test", "test");
+        System.out.println(map.remove("test"));
+
     }
 }
