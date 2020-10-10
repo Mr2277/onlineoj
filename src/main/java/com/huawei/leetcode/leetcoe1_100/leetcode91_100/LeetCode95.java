0060acb634ba241913cdd579f6aa56cc791c71a8
sun <837544713@qq.com>
2020-10-10 22:23:27 +0800

    add LeetCode95

---
+++ src/main/java/com/huawei/leetcode/leetcoe1_100/leetcode91_100/LeetCode95.java
@@ -0,0 +1,141 @@
+package com.huawei.leetcode.leetcoe1_100.leetcode91_100;
+
+import java.util.*;
+
+public class LeetCode95 {
+
+   static class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+  }
+
+  public static List<TreeNode> generateTrees(int n) {
+        List<TreeNode> nodes = new ArrayList<>();
+        if (n == 0) {
+            return nodes;
+        }
+        boolean[] flag = new boolean[n];
+        List<Integer> subResult = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        result = permutation(n, flag, subResult, result);
+        Set<String> preStrSet = new HashSet<>();
+        for (List<Integer> list : result) {
+            TreeNode node = (createTree(list));
+            StringBuilder stringBuilder = new StringBuilder();
+            String preOrderStr = preString(node, stringBuilder);
+            if (preStrSet.isEmpty() || !preStrSet.contains(preOrderStr)) {
+                nodes.add(node);
+                preStrSet.add(preOrderStr);
+            }
+        }
+        return nodes;
+    }
+
+    public  static TreeNode createTree(List<Integer> nodes) {
+        TreeNode root = null;
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        for (Integer integer : nodes) {
+            if (root == null) {
+                root = new TreeNode(integer);
+                root.left = null;
+                root.right = null;
+                treeNodeStack.add(root);
+            } else {
+                TreeNode node = treeNodeStack.peek();
+                if (integer > node.val) {
+                    if (node.right == null) {
+                        TreeNode rightNode = new TreeNode(integer);
+                        rightNode.right = null;
+                        rightNode.left = null;
+                        node.right = rightNode;
+                    }
+                    else {
+                        treeNodeStack.add(node.right);
+                        node = treeNodeStack.peek();
+                        if (integer > node.val) {
+                            TreeNode rightNode = new TreeNode(integer);
+                            rightNode.right = null;
+                            rightNode.left = null;
+                            node.right = rightNode;
+                        } else {
+                            TreeNode leftNode = new TreeNode(integer);
+                            leftNode.right = null;
+                            leftNode.left = null;
+                            node.left = leftNode;
+                        }
+                    }
+                } else {
+                    if (node.left == null) {
+                        TreeNode leftNode = new TreeNode(integer);
+                        leftNode.left = null;
+                        leftNode.right = null;
+                        node.left = leftNode;
+                    } else {
+                        treeNodeStack.add(node.left);
+                        node = treeNodeStack.peek();
+                        if (integer > node.val) {
+                            TreeNode rightNode = new TreeNode(integer);
+                            rightNode.left = null;
+                            rightNode.right = null;
+                            node.right = rightNode;
+                        } else {
+                            TreeNode leftNode = new TreeNode(integer);
+                            leftNode.left = null;
+                            leftNode.right = null;
+                            node.left = leftNode;
+                        }
+                    }
+                }
+            }
+
+        }
+        return root;
+    }
+
+    public static List<List<Integer>> permutation(int n, boolean flag[], List<Integer> subResult, List<List<Integer>> result) {
+        if (subResult.size() == n) {
+            List<Integer> copySubResult = new ArrayList<>(subResult);
+            result.add(copySubResult);
+            return result;
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (!flag[i]) {
+                    flag[i] = true;
+                    subResult.add(i + 1);
+                    permutation(n, flag, subResult, result);
+                    subResult.remove(subResult.size() - 1);
+                    flag[i] = false;
+                }
+            }
+            return result;
+        }
+    }
+
+
+    public static String preString(TreeNode root, StringBuilder stringBuilder) {
+        if (root == null) {
+            return stringBuilder.toString();
+        } else {
+            stringBuilder.append(root.val);
+            preString(root.left, stringBuilder);
+            preString(root.right, stringBuilder);
+            return stringBuilder.toString();
+        }
+    }
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       while (scanner.hasNext()) {
+           int n = scanner.nextInt();
+           generateTrees(n);
+       }
+    }
+
+}
