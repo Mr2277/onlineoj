15dff321e46a7dbe8ff9fce50e138be45e7acb4d
sun <837544713@qq.com>
2020-10-17 12:32:13 +0800

    add Offer55I

---
+++ src/main/java/com/huawei/leetcode/jianqiaooffer/Offer55I.java
@@ -0,0 +1,22 @@
+package com.huawei.leetcode.jianqiaooffer;
+
+public class Offer55I {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Integer.max(left, right) + 1;
+    }
+}
--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode111_120/LeetCode114.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode111_120/LeetCode114.java
@@ -1,4 +1,107 @@
 package com.huawei.leetcode.leetcode1_1000.leetcode101_200.leetcode111_120;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class LeetCode114 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode create(String str) {
+        int index = 0;
+        char ch = str.charAt(index);
+        String cur = "", pre = "", next = "";
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode parent = null, node = null;
+        while (index < str.length()) {
+            while (ch >= '0' && ch <= '9' || ch == '#') {
+                cur += ch;
+                index++;
+                ch = str.charAt(index);
+            }
+            next = String.valueOf(ch);
+            if (!"".equals(cur) && !cur.equals("#")) {
+                Integer curVal = Integer.valueOf(cur);
+                node = new TreeNode(curVal);
+                node.left = null;
+                node.right = null;
+            }
+            switch (pre) {
+
+                case "(":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.left = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ",":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.right = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ")":
+                    treeNodeStack.pop();
+                    break;
+                default:
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+            }
+
+            pre = "".equals(cur) ? String.valueOf(str.charAt(index)) : next;
+            cur = "";
+            index++;
+            ch = index < str.length() ? str.charAt(index) : ')';
+        }
+        return treeNodeStack.peek();
+    }
+
+    public static void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        } else {
+            DFS(root);
+        }
+    }
+
+    public static TreeNode DFS(TreeNode root) {
+        if (root == null) {
+            return root;
+        } else {
+            TreeNode left = DFS(root.left);
+            TreeNode right = DFS(root.right);
+            root.right = left;
+            root.left = null;
+            if (left == null) {
+                root.right = right;
+            } else {
+                left.right = right;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            TreeNode root = create(str);
+            flatten(root);
+        }
+    }
 }
