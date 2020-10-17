f655f96be7f3ad1782459979a3cb0b9a11a7bf66
sun <837544713@qq.com>
2020-10-17 13:05:46 +0800

    add LeetCode199

---
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode191_200/LeetCode199.java
@@ -0,0 +1,101 @@
+package com.huawei.leetcode.leetcode1_1000.leetcode101_200.leetcode191_200;
+
+import java.util.*;
+
+public class LeetCode199 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode create(String str) {
+        int index = 0;
+        char ch = str.charAt(index);
+        String cur = "", pre = "", next = "";
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode parent = null, node = null;
+        while (index < str.length()) {
+            while (ch >= '0' && ch <= '9' || ch == '#') {
+                cur += ch;
+                index++;
+                ch = str.charAt(index);
+            }
+            next = String.valueOf(ch);
+            if (!"".equals(cur) && !cur.equals("#")) {
+                Integer curVal = Integer.valueOf(cur);
+                node = new TreeNode(curVal);
+                node.left = null;
+                node.right = null;
+            }
+            switch (pre) {
+
+                case "(":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.left = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ",":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.right = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ")":
+                    treeNodeStack.pop();
+                    break;
+                default:
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+            }
+
+            pre = "".equals(cur) ? String.valueOf(str.charAt(index)) : next;
+            cur = "";
+            index++;
+            ch = index < str.length() ? str.charAt(index) : ')';
+        }
+        return treeNodeStack.peek();
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> levelNodeMap = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        DFS(root, levelNodeMap, 1, result);
+        return result;
+    }
+
+    public static List<Integer> DFS(TreeNode root, Map<Integer, Integer> levelNodeMap, int level, List<Integer> result) {
+        if (root == null) {
+            return result;
+        } else {
+            if (!levelNodeMap.containsKey(level)) {
+                result.add(root.val);
+                levelNodeMap.put(level, root.val);
+            }
+            DFS(root.right, levelNodeMap, level + 1, result);
+            DFS(root.left, levelNodeMap, level + 1, result);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            TreeNode root = create(str);
+        }
+    }
+}
