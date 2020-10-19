53ffdfa10b429d8b9ebc221cedd234d713e16a43
sun <837544713@qq.com>
2020-10-19 22:56:37 +0800

    add LeetCode965

---
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode901_1000/leetcode961_970/LeetCode965.java
@@ -0,0 +1,91 @@
+package com.huawei.leetcode.leetcode1_1000.leetcode901_1000.leetcode961_970;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class LeetCode965 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode create(String str) {
+        int index = 0;
+        char ch = str.charAt(index);
+        String cur = "", pre = "", next = "";
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode parent = null, node = null;
+        while (index < str.length()) {
+            while (ch >= '0' && ch <= '9' || ch == '#') {
+                cur += ch;
+                index++;
+                ch = str.charAt(index);
+            }
+            next = String.valueOf(ch);
+            if (!"".equals(cur) && !cur.equals("#")) {
+                Integer curVal = Integer.valueOf(cur);
+                node = new TreeNode(curVal);
+                node.left = null;
+                node.right = null;
+            }
+            switch (pre) {
+
+                case "(":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.left = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ",":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.right = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ")":
+                    treeNodeStack.pop();
+                    break;
+                default:
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+            }
+
+            pre = "".equals(cur) ? String.valueOf(str.charAt(index)) : next;
+            cur = "";
+            index++;
+            ch = index < str.length() ? str.charAt(index) : ')';
+        }
+        return treeNodeStack.peek();
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        Set<Integer> nodeSet = new HashSet<>();
+        return DFS(root, nodeSet);
+    }
+
+    public boolean DFS(TreeNode root, Set<Integer> nodeSet) {
+        if (nodeSet.size() > 1) {
+            return false;
+        }
+        if (root == null) {
+            return true;
+        } else {
+            nodeSet.add(root.val);
+            return DFS(root.left, nodeSet) && DFS(root.right, nodeSet);
+        }
+    }
+}
