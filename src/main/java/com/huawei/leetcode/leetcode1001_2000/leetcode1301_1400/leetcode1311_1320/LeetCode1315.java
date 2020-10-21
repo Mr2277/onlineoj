5d79e5b528b43a49270025cc731322cedbb58963
sun <837544713@qq.com>
2020-10-21 23:30:56 +0800

    add LeetCode1315

--- src/main/java/com/huawei/leetcode/leetcode1001_2000/leetcode1301_1400/leetcode1311_1320/LeetCode1315.java
+++ src/main/java/com/huawei/leetcode/leetcode1001_2000/leetcode1301_1400/leetcode1311_1320/LeetCode1315.java
@@ -1,5 +1,7 @@
 package com.huawei.leetcode.leetcode1001_2000.leetcode1301_1400.leetcode1311_1320;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -71,11 +73,62 @@ public class LeetCode1315 {
         return treeNodeStack.peek();
     }
 
+    public static int sumEvenGrandparent(TreeNode root) {
+        List<Integer> nodes = new ArrayList<>();
+        DFS(root, nodes);
+        int sum = 0;
+        for (Integer integer : nodes) {
+            sum += integer;
+        }
+        return sum;
+    }
+
+    public static List<Integer> DFS(TreeNode root, List<Integer> nodes) {
+        if (root == null) {
+            return nodes;
+        } else {
+            if ((root.val & 1) != 1) {
+                TreeNode leftNode = root.left;
+                TreeNode rightNode = root.right;
+                if (leftNode != null) {
+                    TreeNode grandLeftNode = leftNode.left;
+                    TreeNode grandRightNode = leftNode.right;
+                    if (grandLeftNode != null) {
+                        nodes.add(grandLeftNode.val);
+                    }
+                    if (grandRightNode != null) {
+                        nodes.add(grandRightNode.val);
+                    }
+                }
+                if (rightNode != null) {
+                    TreeNode grandLeftNode = rightNode.left;
+                    TreeNode grandRightNode = rightNode.right;
+                    if (grandLeftNode != null) {
+                        nodes.add(grandLeftNode.val);
+                    }
+                    if (grandRightNode != null) {
+                        nodes.add(grandRightNode.val);
+                    }
+                }
+            }
+            DFS(root.left, nodes);
+            DFS(root.right, nodes);
+            return nodes;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String str = scanner.nextLine();
             TreeNode root = create(str);
+            System.out.println(sumEvenGrandparent(root));
         }
+        /*
+        int n = 2;
+        System.out.println(n & 1);
+        n = 3;
+        System.out.println(n & 1);
+        */
     }
 }
