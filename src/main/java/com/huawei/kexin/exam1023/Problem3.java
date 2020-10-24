509f6d44f660385ca0255df5fb057f22db1aaccd
sun <837544713@qq.com>
2020-10-24 08:11:42 +0800

    add Problem3

--- src/main/java/com/huawei/kexin/exam1023/Problem3.java
+++ src/main/java/com/huawei/kexin/exam1023/Problem3.java
@@ -3,7 +3,7 @@ package com.huawei.kexin.exam1023;
 import java.util.*;
 
 public class Problem3 {
-
+    /*
     public static Set<String> alreadySearchSet = new HashSet<>();
 
     public static int maxSum(int[] nums, int step, int back, int index) {
@@ -52,6 +52,60 @@ public class Problem3 {
         }
         return result;
     }
+    */
+    static class CurrentInfo {
+       int index;
+       int sum;
+       int step;
+       int back;
+       CurrentInfo (int index, int sum, int step, int back) {
+           this.back = back;
+           this.index = index;
+           this.step = step;
+           this.sum = sum;
+       }
+    }
+
+    public static int maxSum(int[] nums, int step, int back, int index) {
+        List<CurrentInfo> currentInfos = new ArrayList<>();
+        CurrentInfo currentInfo = new CurrentInfo(index, nums[index], step, back);
+        currentInfos.add(currentInfo);
+        int backCopy = back;
+        int stepCopy = step;
+        int front = 0, rear = 1;
+        while (rear > front) {
+            CurrentInfo cur = currentInfos.get(front++);
+            int curBack = cur.back;
+            int curStep = cur.step;
+            int curIndex = cur.index;
+            int curSum = cur.sum;
+            if (curBack > 0 && curStep > 0) {
+                int newBack = curBack - 1;
+                int newStep = curStep - 1;
+                int newIndex = curIndex - 1;
+                int newSum = curSum + nums[newIndex];
+                CurrentInfo newCurentInfo = new CurrentInfo(newIndex, newSum, newStep, newBack);
+                currentInfos.add(newCurentInfo);
+                rear++;
+            }
+            if (curStep > 0) {
+                int newBack = curBack;
+                int newStep = curStep - 1;
+                int newIndex = curIndex + 1;
+                int newSum = curSum + nums[newIndex];
+                CurrentInfo newCurrentInfo = new CurrentInfo(newIndex, newSum, newStep, newBack);
+                currentInfos.add(newCurrentInfo);
+                rear++;
+            }
+        }
+        for (CurrentInfo currentInfo1 : currentInfos) {
+            System.out.println("index:" + currentInfo1.index + "sum:" + currentInfo1.sum
+            + "step:" + currentInfo1.step + "back:" + currentInfo1.back);
+        }
+        return 0;
+        }
+        //return 0;
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
