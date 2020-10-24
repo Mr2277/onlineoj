782d6ac0f985dd4ed13afe65eccb182d8183f8b2
sun <837544713@qq.com>
2020-10-24 14:52:37 +0800

    Probelem1619

--- src/main/java/com/huawei/leetcode/interview/Problem1619.java
+++ src/main/java/com/huawei/leetcode/interview/Problem1619.java
@@ -14,7 +14,7 @@ public class Problem1619 {
             this.y = y;
         }
     }
-
+    /*
     public static int[] pondSizes(int[][] land) {
         List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < land.length; i++) {
@@ -45,6 +45,73 @@ public class Problem1619 {
         int left = DFS(land, x, y - 1, num);
         int leftUp = DFS(land, x - 1, y - 1, num);
         return up + right + down + left + leftDown + leftUp + rightDown + rihtUp + 1;
+    }
+    */
+
+    public static int[] pondSizes(int[][] land) {
+        List<List<Location>> paths = new ArrayList<>();
+        List<Location> path = new ArrayList<>();
+        Set<String> alreadySearchSet = new HashSet<>();
+        List<List<Location>> copyPaths = new ArrayList<>();
+        for (int i = 0; i < land.length; i++) {
+            for (int j = 0; j < land[0].length; j++) {
+                if (land[i][j] == 0) {
+                    List<Location> list = new ArrayList<>();
+                    DFS(paths, path, land, i, j);
+                    for (List<Location> locations : paths) {
+                        for (Location location : locations) {
+                            //System.out.print(location.x + "#" + location.y + " ");
+                            String key = "x:" + String.valueOf(location.x) + " y:" + String.valueOf(location.y);
+                            if (!alreadySearchSet.contains(key)) {
+                                list.add(location);
+                                alreadySearchSet.add(key);
+                            }
+                        }
+                    }
+                    copyPaths.add(list);
+
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (List<Location> locations : copyPaths) {
+            /*
+            for (Location location : locations) {
+                System.out.print(location.x + "#" + location.y + " ");
+            }
+            System.out.println();
+            */
+            result.add(locations.size());
+        }
+        int[] arr1 = result.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return arr1;
+    }
+
+    public static List<List<Location>> DFS(List<List<Location>> paths, List<Location> path, int[][] land, int x, int y) {
+        int rows = land.length;
+        int cols = land[0].length;
+        if (x < 0 || x >= rows || y < 0 || y >= cols || land[x][y] != 0) {
+            return null;
+        }
+        Location location = new Location(x, y);
+        path.add(location);
+        land[x][y] = -1;
+        List<List<Location>> up = DFS(paths, path, land,x - 1 ,y);
+        List<List<Location>> rightUp = DFS(paths, path, land,x - 1, y + 1);
+        List<List<Location>> right = DFS(paths, path, land, x, y + 1);
+        List<List<Location>> rightDown = DFS(paths, path, land,x + 1, y + 1);
+        List<List<Location>> down = DFS(paths, path, land,x + 1, y);
+        List<List<Location>> leftDown = DFS(paths, path, land,x + 1, y - 1);
+        List<List<Location>> left= DFS(paths, path, land, x, y - 1);
+        List<List<Location>> leftUp = DFS(paths, path, land,x - 1, y - 1);
+        if (up == null && rightUp == null && right == null && rightDown == null && down == null && leftDown == null && left == null && leftUp == null) {
+            List<Location> copyPath = new ArrayList<>(path);
+            paths.add(copyPath);
+        } else {
+
+        }
+        path.remove(path.size() - 1);
+        return paths;
     }
 
     public static void main(String[] args) {
