b76af78c7b53050e381acb83b590445bf4703a52
sun <837544713@qq.com>
2020-10-24 19:32:33 +0800

    add Problem0404

---
+++ src/main/java/com/huawei/leetcode/interview/Problem0404.java
@@ -0,0 +1,101 @@
+package com.huawei.leetcode.interview;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Problem0404 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode create(String str) {
+        int index = 0;
+        char ch = str.charAt(index);
+        String cur = "", pre = "", next = "";
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode parent = null, node = null;
+        while (index < str.length()) {
+            while (ch >= '0' && ch <= '9' || ch == '#') {
+                cur += ch;
+                index++;
+                ch = str.charAt(index);
+            }
+            next = String.valueOf(ch);
+            if (!"".equals(cur) && !cur.equals("#")) {
+                Integer curVal = Integer.valueOf(cur);
+                node = new TreeNode(curVal);
+                node.left = null;
+                node.right = null;
+            }
+            switch (pre) {
+
+                case "(":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.left = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ",":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.right = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ")":
+                    treeNodeStack.pop();
+                    break;
+                default:
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+            }
+
+            pre = "".equals(cur) ? String.valueOf(str.charAt(index)) : next;
+            cur = "";
+            index++;
+            ch = index < str.length() ? str.charAt(index) : ')';
+        }
+        return treeNodeStack.peek();
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        treeDepth(root);
+        return flag;
+    }
+
+    public static boolean flag = true;
+
+    public static int treeDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = treeDepth(root.left);
+        int right = treeDepth(root.right);
+        if (left - right < -1 || left - right > 1) {
+            flag = false;
+        }
+        return Integer.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            TreeNode root = create(str);
+            System.out.println(isBalanced(root));
+        }
+    }
+}
