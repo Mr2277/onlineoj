822387e709d9d60bfe76312cc843edbe689b8f05
sun <837544713@qq.com>
2020-10-31 23:02:19 +0800

    add null

--- src/main/java/com/huawei/test/TestContent.java
+++ src/main/java/com/huawei/test/TestContent.java
@@ -2,11 +2,7 @@ package com.huawei.test;
 
 public class TestContent {
     public static void main(String[] args) {
-        /*
-       String str = "\\d+(.\\d+)*";
-       String s = "3.4";
-       System.out.println(s.matches(str));
-       */
+
         String str = "apache jatestjva";
         System.out.println(str.contains("java"));
     }
