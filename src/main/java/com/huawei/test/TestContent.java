84495183104c6061c693b1826b2545731955cbce
sun <837544713@qq.com>
2020-11-01 09:23:50 +0800

    Problem0406

--- src/main/java/com/huawei/test/TestContent.java
+++ src/main/java/com/huawei/test/TestContent.java
@@ -2,7 +2,6 @@ package com.huawei.test;
 
 public class TestContent {
     public static void main(String[] args) {
-
         String str = "apache jatestjva";
         System.out.println(str.contains("java"));
     }
