1688feea00ee6e78a57a73ed4d1c2dfede0dd318
sun <837544713@qq.com>
2020-11-09 23:27:38 +0800

    add LeetCode576

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode501_600/leetcode571_580/LeetCode576.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode501_600/leetcode571_580/LeetCode576.java
@@ -3,28 +3,33 @@ package com.huawei.leetcode.leetcode1_1000.leetcode501_600.leetcode571_580;
 public class LeetCode576 {
 
     public static int findPaths(int m, int n, int N, int i, int j) {
-        return 0;
+        int[][] grid = new int[m][n];
+        return DFS(grid, i, j, N);
     }
 
     public static int DFS(int[][] grid, int x, int y, int step) {
-        if (step <= 0 || grid[x][y] == -1) {
+        if (step < 0) {
             return 0;
         }
         if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
             return 1;
         }
+        if (grid[x][y] == -1) {
+            return 0;
+        }
         int res = 0;
         int[][] dir = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        grid[x][y] = -1;
+        //grid[x][y] = -1;
         for (int[] d : dir) {
             int nextX = x + d[0];
             int nextY = y + d[1];
             res += DFS(grid, nextX, nextY, step - 1);
         }
+        //grid[x][y] = 0;
         return res;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(findPaths(1, 3, 3, 0, 1));
     }
 }
