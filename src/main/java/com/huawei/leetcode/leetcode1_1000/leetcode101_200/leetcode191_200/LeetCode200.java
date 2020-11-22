b0a6cb3cd78e920b7cd46ee1112f7fe920d760c2
sun <837544713@qq.com>
2020-11-22 23:29:57 +0800

    add LeetCode200

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode191_200/LeetCode200.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode191_200/LeetCode200.java
@@ -1,7 +1,12 @@
 package com.huawei.leetcode.leetcode1_1000.leetcode101_200.leetcode191_200;
 
 public class LeetCode200 {
-
-
-
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+    }
 }
