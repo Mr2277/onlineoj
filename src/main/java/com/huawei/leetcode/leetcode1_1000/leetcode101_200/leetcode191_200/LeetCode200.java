82da14335c4b8e1e8089d5405b6d696b3256323c
sun <837544713@qq.com>
2020-11-23 23:16:14 +0800

    add LeetCode200

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode191_200/LeetCode200.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode101_200/leetcode191_200/LeetCode200.java
@@ -1,6 +1,33 @@
 package com.huawei.leetcode.leetcode1_1000.leetcode101_200.leetcode191_200;
 
 public class LeetCode200 {
+
+    public static int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] != '1') {
+            return;
+        }
+        grid[x][y] = '2';
+        int[][] dir = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int[] d : dir) {
+            int nextX = x + d[0];
+            int nextY = y + d[1];
+            dfs(grid, nextX, nextY);
+        }
+    }
+
     public static void main(String[] args) {
         char[][] grid = new char[][]{
                 {'1', '1', '1', '1', '0'},
@@ -8,5 +35,6 @@ public class LeetCode200 {
                 {'1', '1', '0', '0', '0'},
                 {'0', '0', '0', '0', '0'}
         };
+        System.out.println(numIslands(grid));
     }
 }
