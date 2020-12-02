46861a1dfdd7fabcf90681d61fd0a512f012f6db
sun <837544713@qq.com>
2020-12-02 23:47:34 +0800

    add LeetCode967

---
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode901_1000/leetcode961_970/LeetCode967.java
@@ -0,0 +1,14 @@
+package com.huawei.leetcode.leetcode1_1000.leetcode901_1000.leetcode961_970;
+
+import java.util.Stack;
+
+public class LeetCode967 {
+    /*
+    public static int[] numsSameConsecDiff(int n, int k) {
+        for (int i = 0; i < )
+    }
+    */
+    public static void main(String[] args) {
+
+    }
+}
