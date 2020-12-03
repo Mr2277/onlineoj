cceed407d9802ad5fd87ad4a593b5f70801c3928
sun <837544713@qq.com>
2020-12-03 23:12:05 +0800

    add LeetCode967

---
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode901_1000/leetcode931_940/LeetCode934.java
@@ -0,0 +1,13 @@
+package com.huawei.leetcode.leetcode1_1000.leetcode901_1000.leetcode931_940;
+
+public class LeetCode934 {
+    public static void main(String[] args) {
+        int[][] A = new int[][] {
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 1},
+                {1, 0, 1, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1}
+        };
+    }
+}
