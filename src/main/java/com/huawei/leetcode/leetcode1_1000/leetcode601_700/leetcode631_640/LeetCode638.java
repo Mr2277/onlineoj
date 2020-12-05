3e80f8807801687c26cd595f43c83726cec601af
sun <837544713@qq.com>
2020-12-05 23:26:37 +0800

    add LeetCode638

---
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode601_700/leetcode631_640/LeetCode638.java
@@ -0,0 +1,15 @@
+package com.huawei.leetcode.leetcode1_1000.leetcode601_700.leetcode631_640;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode638 {
+    public static void main(String[] args) {
+        List<Integer> price = new ArrayList<>();
+        List<List<Integer>> special = new ArrayList<>();
+        List<Integer> needs = new ArrayList<>();
+        price.add(2);
+        price.add(5);
+
+    }
+}
