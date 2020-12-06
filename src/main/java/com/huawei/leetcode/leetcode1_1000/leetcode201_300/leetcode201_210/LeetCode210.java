53c438a0fcbc9f45ed188ef7499b190f2dbe2ca7
sun <837544713@qq.com>
2020-12-06 20:45:37 +0800

    add LeetCode210

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode201_300/leetcode201_210/LeetCode210.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode201_300/leetcode201_210/LeetCode210.java
@@ -51,74 +51,6 @@ public class LeetCode210 {
         return new int[numCourses];
     }
     */
-    public boolean isXunHuan = false;
-    public int[] findOrder(int num, int[][] prerequisites) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        boolean[] flag = new boolean[num];
-        for (int[] pre : prerequisites) {
-            if (!map.containsKey(pre[1])) {
-                List<Integer> list = new ArrayList<>();
-                list.add(pre[0]);
-                flag[pre[0]] = true;
-                map.put(pre[1], list);
-            } else {
-                List<Integer> list = map.get(pre[1]);
-                list.add(pre[0]);
-                flag[pre[0]] = true;
-                map.put(pre[1], list);
-            }
-        }
-        int cur = 0;
-        for (int i = 0; i < num; i++) {
-            if (!flag[i]) {
-                cur = i;
-            }
-        }
-        int[] colors = new int[num];
-        dfs(map, colors, cur);
-        if (isXunHuan) {
-            return new int[]{};
-        } else {
-            List<Integer> list = new ArrayList<>();
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(cur);
-            while (!queue.isEmpty()) {
-                int size = queue.size();
-                for (int i = 0; i < size; i++) {
-                    cur = queue.poll();
-                    list.add(cur);
-                    colors[cur] = 0;
-                    if (map.containsKey(cur)) {
-                        List<Integer> value = map.get(cur);
-                        for (Integer integer : value) {
-                            if (colors[integer] == 1) {
-                                queue.add(integer);
-                                colors[integer] = 0;
-                            }
-                        }
-                    }
-                }
-            }
-            int[] arr1 = list.stream().mapToInt(Integer::valueOf).toArray();
-            return arr1;
-        }
-    }
-
-    public void dfs(Map<Integer, List<Integer>> map, int[] colors, int cur) {
-        if (colors[cur] == 0) {
-            colors[cur] = -1;
-            if (map.containsKey(cur)) {
-                List<Integer> list = map.get(cur);
-                for (Integer integer : list) {
-                    dfs(map, colors, integer);
-                }
-            }
-            colors[cur] = 1;
-        }
-        if (colors[cur] == -1) {
-            isXunHuan = true;
-        }
-    }
 
     /*
     public int[] findOrder(int num, int[][] prerequisites) {
@@ -171,9 +103,47 @@ public class LeetCode210 {
         return new int[] {};
     }
     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        Queue<Integer> queue = new LinkedList<>();
+        //创建入度表和哈希表
+        for (int i = 0; i < prerequisites.length; i++) {
+            inDegree[prerequisites[i][0]]++;
+            if(map.containsKey(prerequisites[i][1])){
+                map.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(prerequisites[i][0]);
+                map.put(prerequisites[i][1], list);
+            }
+        }
+        //遍历，将index入队
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if(inDegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+        // 出队，查哈希表，将入度为零的入队
+        while (!queue.isEmpty()){
+            Integer cur = queue.poll();
+            res.add(cur);
+            if(map.containsKey(cur) && map.get(cur).size() != 0){
+                for (Integer num : map.get(cur)) {
+                    inDegree[num]--;
+                    if(inDegree[num] == 0) queue.offer(num);
+                }
+            }
+        }
+        //使用list的流来转为int[]数组，也可以通过遍历一遍完成转换。
+        return res.size() == numCourses ? res.stream().mapToInt(Integer::valueOf).toArray() : new int[0];
+
+    }
     public static void main(String[] args) {
-        int[][] pre = new int[][] {{1,0},{2,0},{3,1},{3,2},{2,3}};
-        int[] result = new LeetCode210().findOrder(4, pre);
+        //int[][] pre = new int[][] {{1,0},{2,0},{3,1},{3,2}};
+        int[][] pre = new int[][] {{1,0}};
+        int[] result = new LeetCode210().findOrder(3, pre);
         for (Integer integer : result) {
             System.out.println(integer);
         }
