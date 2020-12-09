7952d8dc1252ec9d45b6cefcd80af2c69445fc19
sun <837544713@qq.com>
2020-12-09 22:01:04 +0800

    add LeetCode210

---
+++ src/main/java/com/huawei/test/TestBFS.java
@@ -0,0 +1,19 @@
+package com.huawei.test;
+
+public class TestBFS {
+    public void bfs(int[][] grid) {
+
+    }
+    public static void main(String[] args) {
+        int[][] grid = new int[][] {
+                {0, 0, 0},
+                {1, 0, 0},
+                {1, 0, 0}
+        };
+        new TestBFS().bfs(grid);
+
+
+
+
+    }
+}
