59ad3929cc81b99a6bbda30cc259ba4ada295f6f
sun <837544713@qq.com>
2020-12-15 22:59:53 +0800

    ADD

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode901_1000/leetcode931_940/LeetCode934.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode901_1000/leetcode931_940/LeetCode934.java
@@ -1,6 +1,45 @@
 package com.huawei.leetcode.leetcode1_1000.leetcode901_1000.leetcode931_940;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LeetCode934 {
+
+    static class Location {
+        int x;
+        int y;
+        int step;
+        Location from;
+        Location(int x, int y, int step, Location from) {
+            this.x = x;
+            this.y = y;
+            this.step = step;
+            this.from = from;
+        }
+    }
+
+    public int shortestBridge(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] == 1) {
+
+                }
+            }
+        }
+    }
+
+    public List<Location> bfs(int[][] A, Location start, int groupId) {
+        Queue<Location> queue = new LinkedList<>();
+        List<Location> locations = new ArrayList<>();
+        queue.add(start);
+        locations.add(start);
+        A[start.x][start.y] = groupId;
+        while (!queue.isEmpty()) {
+
+        }
+    }
     public static void main(String[] args) {
         int[][] A = new int[][] {
                 {1, 1, 1, 1, 1},
