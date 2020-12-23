c5f351a38eb49451bec52b07d27a0f93c51a5666
sun <837544713@qq.com>
2020-12-23 22:56:24 +0800

    add LeetCode787

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode701_800/leetcode781_790/LeetCode787.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode701_800/leetcode781_790/LeetCode787.java
@@ -30,34 +30,65 @@ public class LeetCode787 {
             String key = flights[i][0] + "->" + flights[i][1];
             priceMap.put(key, flights[i][2]);
         }
-        System.out.println(priceMap.size());
-        System.out.println(flightMap.size());
         Location cur = new Location(src, K, 0);
         Queue<Location> queue = new LinkedList<>();
         queue.add(cur);
+        List<Location> result = new ArrayList<>();
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 cur = queue.poll();
-                for (Integer nextAirport : flightMap.get(cur.airport)) {
-                    Integer cost = priceMap.get(cur.airport + "->" + nextAirport);
-                    if (dst == nextAirport) {
-                        Location next = new Location(nextAirport, K, cost + cur.costs);
-                        queue.add(next);
-                    } else {
-                        K--;
-                        if (K >= 0) {
+                if (!flightMap.get(cur.airport).isEmpty()) {
+                    for (Integer nextAirport : flightMap.get(cur.airport)) {
+                        Integer cost = priceMap.get(cur.airport + "->" + nextAirport);
+                        if (dst == nextAirport) {
                             Location next = new Location(nextAirport, K, cost + cur.costs);
+                            // queue.add(next);
+                            result.add(next);
+                        } else {
+                            K--;
+                            if (K >= 0) {
+                                Location next = new Location(nextAirport, K, cost + cur.costs);
+                                queue.add(next);
+                            }
                         }
                     }
                 }
             }
         }
-        return 0;
+        int min = Integer.MAX_VALUE;
+        for (Location location : result) {
+            if (location.costs < min) {
+                min = location.costs;
+            }
+        }
+        if (min == Integer.MAX_VALUE) {
+            min = -1;
+        }
+        return min;
     }
 
     public static void main(String[] args) {
-        int[][] edges = new int[][] {{0, 1, 100}, {1, 2, 100},{0, 2, 500}};
-        new LeetCode787().findCheapestPrice(3, edges, 0, 2, 1);
+        //5 [[4,1,1],[1,2,3],[0,3,2],[0,4,10],[3,1,1],[1,4,3]] 2 1 1
+        /*
+        int[][] edges = new int[][] {
+                {4, 1, 1},
+                {1, 2, 3},
+                {0, 3, 2},{0,4,10],[3,1,1],[1,4,3]};
+        new LeetCode787().findCheapestPrice(3, edges, 0, 2, 0);
+        */
+        /*&
+        short s = 1;
+        s = (short) (s + 1);
+        System.out.println(s);
+        */
+        Map<Integer, String> map = new LinkedHashMap<>(1, 0.75f, true);
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        map.get(1);
+        for (Map.Entry entry : map.entrySet()) {
+            System.out.println((Integer) entry.getKey() + (String)entry.getValue());
+        }
     }
 }
