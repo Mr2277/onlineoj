8644647d2b4b5fd7a2f80cfc71ff3344d309762e
sun <837544713@qq.com>
2020-12-27 09:06:57 +0800

    add LeetCode787

--- src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode701_800/leetcode781_790/LeetCode787.java
+++ src/main/java/com/huawei/leetcode/leetcode1_1000/leetcode701_800/leetcode781_790/LeetCode787.java
@@ -14,81 +14,64 @@ public class LeetCode787 {
        }
     }
 
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        Map<Integer, List<Integer>> flightMap = new HashMap<>();
-        Map<String, Integer> priceMap = new HashMap<>();
-        for (int i = 0; i < flights.length; i++) {
-            if (!flightMap.containsKey(flights[i][0])) {
-                List<Integer> list = new ArrayList<>();
-                list.add(flights[i][1]);
-                flightMap.put(flights[i][0], list);
-            } else {
-                List<Integer> list = flightMap.get(flights[i][0]);
-                list.add(flights[i][1]);
-                flightMap.put(flights[i][0], list);
-            }
-            String key = flights[i][0] + "->" + flights[i][1];
-            priceMap.put(key, flights[i][2]);
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+    {
+        int[][] g = new int[n][n];//记录的是目的地
+        for(int[] f : flights)
+        {
+            g[f[0]][f[1]] = f[2];
         }
-        Location cur = new Location(src, K, 0);
-        Queue<Location> queue = new LinkedList<>();
-        queue.add(cur);
-        List<Location> result = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                cur = queue.poll();
-                if (!flightMap.get(cur.airport).isEmpty()) {
-                    for (Integer nextAirport : flightMap.get(cur.airport)) {
-                        Integer cost = priceMap.get(cur.airport + "->" + nextAirport);
-                        if (dst == nextAirport) {
-                            Location next = new Location(nextAirport, K, cost + cur.costs);
-                            // queue.add(next);
-                            result.add(next);
-                        } else {
-                            K--;
-                            if (K >= 0) {
-                                Location next = new Location(nextAirport, K, cost + cur.costs);
-                                queue.add(next);
-                            }
-                        }
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b)->a[0] - b[0]);
+        //集合的参数是一个comparator的lambda表达式，默认升序
+        heap.offer(new int[]{0, src, K + 1});//想集合添加一个记录费用、起点和中转站+1的数组
+        //K + 1是还可以走过站点的个数
+
+        while(!heap.isEmpty())//数组为空直接返回-1
+        {
+            int[] cur = heap.poll();//得到集合当中添加的数组
+            int price = cur[0], place = cur[1], remainStops = cur[2];
+
+            if(place == dst)//起点等于v中点
+                return price;//返回0费用0
+
+            if(remainStops > 0)//中转次数》0（至少执行一次，因为remain====k+1）
+            {
+                for(int i = 0; i < n; i++)//小于城市数量
+                {
+                    if(g[place][i] > 0)//表示起点----终点的中转路线是否存在
+                    {
+                        heap.offer(new int[]{price + g[place][i], i, remainStops - 1});
+                        //如果存在 计算路费、起点的值、中转站-1
                     }
                 }
             }
         }
-        int min = Integer.MAX_VALUE;
-        for (Location location : result) {
-            if (location.costs < min) {
-                min = location.costs;
-            }
-        }
-        if (min == Integer.MAX_VALUE) {
-            min = -1;
-        }
-        return min;
+
+        return -1;
     }
 
     public static void main(String[] args) {
-        //5 [[4,1,1],[1,2,3],[0,3,2],[0,4,10],[3,1,1],[1,4,3]] 2 1 1
+
+
         /*
         int[][] edges = new int[][] {
                 {4, 1, 1},
                 {1, 2, 3},
-                {0, 3, 2},{0,4,10],[3,1,1],[1,4,3]};
-        new LeetCode787().findCheapestPrice(3, edges, 0, 2, 0);
+                {0, 3, 2},
+                {0, 4, 10},
+                {3, 1, 1},
+                {1, 4, 3}
+        };
+        new LeetCode787().findCheapestPrice(5, edges, 2, 1, 1);
         */
-        /*&
-        short s = 1;
-        s = (short) (s + 1);
-        System.out.println(s);
-        */
-        Map<Integer, String> map = new LinkedHashMap<>(1, 0.75f, true);
-        map.put(1, "a");
-        map.put(2, "b");
-        map.put(3, "c");
-        map.get(1);
-        for (Map.Entry entry : map.entrySet()) {
-            System.out.println((Integer) entry.getKey() + (String)entry.getValue());
-        }
+        //4 [[0,1,1],[0,2,5],[1,2,1],[2,3,1]] 0 3 1
+        int[][] edges = new int[][] {
+                {0, 1, 1},
+                {0, 2, 5},
+                {1, 2, 1},
+                {2, 3, 1}
+        };
+        new LeetCode787().findCheapestPrice(4, edges, 0, 3, 1);
     }
 }
