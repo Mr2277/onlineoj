247be29f007401344642aaf6341f8b7de8be09a0
sun <837544713@qq.com>
2021-01-23 21:26:22 +0800

    add LeetCode542

--- src/main/java/com/huawei/kexin/leetcodebfs/LeetCode542.java
+++ src/main/java/com/huawei/kexin/leetcodebfs/LeetCode542.java
@@ -1,6 +1,8 @@
 package com.huawei.kexin.leetcodebfs;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LeetCode542 {
@@ -18,11 +20,13 @@ public class LeetCode542 {
 
     public int[][] updateMatrix(int[][] matrix) {
         Queue<Location> queue = new LinkedList<>();
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
                     Location location = new Location(i, j,0);
                     queue.add(location);
+                    list.add(i + "@" + j);
                 }
             }
         }
@@ -34,17 +38,36 @@ public class LeetCode542 {
                 for (int[] d : dir) {
                     int nextX = cur.x + d[0];
                     int nextY = cur.y + d[1];
-                    if (nextX >= 0 && nextX < matrix.length && nextY >= 0 && nextY < matrix[0].length && matrix[nextX][nextY] != 0) {
-                        int target = cur.target + 1;
-                        if (target <= matrix[nextX][nextY]) {
-                            matrix[nextX][nextY] = target;
-                            Location next = new Location(nextX, nextY, target);
+                    if (nextX >= 0 && nextX < matrix.length && nextY >= 0 && nextY < matrix[0].length) {
+                        if (!list.contains(nextX + "@" + nextY)) {
+                            if (matrix[nextX][nextY] == 0) {
+                                matrix[cur.x][cur.y] = 1;
+                            } else {
+                                matrix[nextX][nextY] = cur.target + 1;
+                            }
+                            Location next = new Location(nextX, nextY, cur.target + 1);
                             queue.add(next);
+                            list.add(nextX + "@" + nextY);
                         }
+
                     }
+
                 }
             }
         }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {
+                {0,1,0,1,1},
+                {1,1,0,0,1},
+                {0,0,0,1,0},
+                {1,0,1,1,1},
+                {1,0,0,0,1}
+        };
+        new LeetCode542().updateMatrix(matrix);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -52,15 +75,5 @@ public class LeetCode542 {
             System.out.println();
         }
         System.out.println();
-        return matrix;
-    }
-
-    public static void main(String[] args) {
-        int[][] matrix = new int[][] {
-                {0, 0, 0},
-                {0, 1, 0},
-                {1, 1, 1}
-        };
-        new LeetCode542().updateMatrix(matrix);
     }
 }
