3f2a388adf6bee6bd36c4d6873544d514c36916f
sun <837544713@qq.com>
2021-01-23 13:10:57 +0800

    MaxNum

--- src/main/java/com/huawei/kexin/exam0122/MaxNum.java
+++ src/main/java/com/huawei/kexin/exam0122/MaxNum.java
@@ -1,13 +1,15 @@
 package com.huawei.kexin.exam0122;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MaxNum {
 
-    public static int composeMaxNum(String A, String B) {
+    public static String composeMaxNum(String A, String B) {
         List<String> newNums = new ArrayList<>();
+        List<BigInteger> bigIntegers = new ArrayList<>();
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i <= A.length(); i++) {
             String left = A.substring(0, i);
@@ -19,10 +21,17 @@ public class MaxNum {
             builder.delete(0, builder.toString().length());
         }
         for (String str : newNums) {
-            System.out.println(str);
+            BigInteger bigInteger = new BigInteger(str);
+            bigIntegers.add(bigInteger);
+            //System.out.println(str);
         }
-        Integer max = newNums.stream().map(str -> Integer.parseInt(str)).max(Integer::compare).get();
-        return max;
+        //Integer max = newNums.stream().map(str -> Integer.parseInt(str)).max(Integer::compare).get();
+        for (BigInteger bigInteger : bigIntegers) {
+            //System.out.println(bigInteger);
+        }
+        BigInteger maxBigInteger = bigIntegers.stream().max(BigInteger::compareTo).get();
+        System.out.println("maxBig:" + maxBigInteger);
+        return maxBigInteger.toString();
     }
 
     public static void main(String[] args) {
