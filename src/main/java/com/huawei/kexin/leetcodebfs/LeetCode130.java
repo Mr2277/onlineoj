657e189c852fd0882e8cd133469ae5511b8eca58
sun <837544713@qq.com>
2021-01-23 19:06:36 +0800

    add LeetCode130

---
+++ src/main/java/com/huawei/kexin/leetcodebfs/LeetCode130.java
@@ -0,0 +1,74 @@
+package com.huawei.kexin.leetcodebfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LeetCode130 {
+
+    static class Location {
+        int x;
+        int y;
+        Location(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public void solve(char[][] board) {
+        Queue<Location> queue = new LinkedList<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if ((i == 0 || i == board.length - 1 || j == 0 || j == board[0].length - 1) && board[i][j] == 'O') {
+                    Location start = new Location(i, j);
+                    queue.add(start);
+                    board[i][j] = 'Y';
+                }
+            }
+        }
+        int[][] dir = new int[][] {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Location cur = queue.poll();
+                for (int[] d : dir) {
+                    int nextX = cur.x + d[0];
+                    int nextY = cur.y + d[1];
+                    if (nextX >= 0 && nextX < board.length && nextY >= 0 && nextY < board[0].length && board[nextX][nextY] == 'O') {
+                        board[nextX][nextY] = 'Y';
+                        Location next = new Location(nextX, nextY);
+                        queue.add(next);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == 'Y') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][] {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'}
+        };
+        new LeetCode130().solve(board);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
