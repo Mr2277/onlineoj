72d419ece8b7eb6739b248e2efe2f9695e0ca0b7
sun <837544713@qq.com>
2021-02-15 22:11:58 +0800

    add LeetCode257

--- src/main/java/com/huawei/kexin/leetcodedfs/LeetCode257.java
+++ src/main/java/com/huawei/kexin/leetcodedfs/LeetCode257.java
@@ -1,4 +1,104 @@
 package com.huawei.kexin.leetcodedfs;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class LeetCode257 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode create(String str) {
+        int index = 0;
+        char ch = str.charAt(index);
+        String cur = "", pre = "", next = "";
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode parent = null, node = null;
+        while (index < str.length()) {
+            while (ch >= '0' && ch <= '9' || ch == '#') {
+                cur += ch;
+                index++;
+                ch = str.charAt(index);
+            }
+            next = String.valueOf(ch);
+            if (!"".equals(cur) && !cur.equals("#")) {
+                Integer curVal = Integer.valueOf(cur);
+                node = new TreeNode(curVal);
+                node.left = null;
+                node.right = null;
+            }
+            switch (pre) {
+
+                case "(":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.left = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ",":
+                    parent = treeNodeStack.peek();
+                    if (!cur.equals("#")) {
+                        parent.right = node;
+                    }
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+                case ")":
+                    treeNodeStack.pop();
+                    break;
+                default:
+                    if (next.equals("(")) {
+                        treeNodeStack.add(node);
+                    }
+                    break;
+            }
+
+            pre = "".equals(cur) ? String.valueOf(str.charAt(index)) : next;
+            cur = "";
+            index++;
+            ch = index < str.length() ? str.charAt(index) : ')';
+        }
+        return treeNodeStack.peek();
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        dfs(root, "", list);
+        return list;
+    }
+
+    public List<String> dfs(TreeNode root, String str, List<String> list) {
+        if (root == null) {
+            return list;
+        }
+        if (root.left == null && root.right == null) {
+            list.add(str + root.val);
+            return list;
+        }
+        dfs(root.left, str + root.val + "->", list);
+        dfs(root.right, str + root.val + "->", list);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        String str = "1(2(#,5),3)";
+        TreeNode root = create(str);
+        List<String> list = new LeetCode257().binaryTreePaths(root);
+        for (String string : list) {
+            System.out.println(string);
+        }
+    }
 }
