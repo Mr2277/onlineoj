7ffa6202937295f14e686adbc060264175856605
sun <837544713@qq.com>
2021-02-21 18:59:56 +0800

    add LeetCode867

---
+++ src/main/java/com/huawei/kexin/array/LeetCode867.java
@@ -0,0 +1,30 @@
+package com.huawei.kexin.array;
+
+public class LeetCode867 {
+
+    public int[][] transpose(int[][] matrix) {
+        int[][] res = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+               res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        matrix = new LeetCode867().transpose(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
