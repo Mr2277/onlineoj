e4fbabde7f4d314a7e2003ade5d2ab2423ff0adb
sun <837544713@qq.com>
2021-02-21 17:33:54 +0800

    add LeetCode494

--- src/main/java/com/huawei/kexin/leetcodedfs/LeetCode494.java
+++ src/main/java/com/huawei/kexin/leetcodedfs/LeetCode494.java
@@ -7,60 +7,21 @@ import java.util.stream.Collectors;
 public class LeetCode494 {
 
     public int findTargetSumWays(int[] nums, int S) {
-        if (nums.length == 1) {
-            int res = 0;
-            if (nums[0] == S) {
-                res++;
-            }
-            if (nums[0] == 0 - S) {
-                res++;
-            }
-            return res;
-        }
-        List<List<Integer>> lists = new ArrayList<>();
-        int[] num = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (j <= i) {
-                    num[j] = 1;
-                }
-            }
-            boolean[] flag = new boolean[nums.length];
-            List<Integer> sub = new ArrayList<>();
-            dfs(num, flag, sub, lists);
-            lists = lists.stream().distinct().collect(Collectors.toList());
-        }
-        int count = 0;
-        for (List<Integer> list : lists) {
-            int sum = 0;
-            for (int i = 0; i < nums.length; i++) {
-                sum = list.get(i) == 1 ? sum + nums[i] : sum - nums[i];
-            }
-            if (sum == S) {
-                count++;
-            }
-        }
-        return count;
+        return dfs(nums, 0, 0, S);
     }
 
-
-    public List<List<Integer>> dfs(int[] num, boolean[] flag, List<Integer> sub, List<List<Integer>> lists) {
-        if (sub.size() == num.length) {
-            List<Integer> copy = new ArrayList<>(sub);
-            lists.add(copy);
-            return lists;
-        }
-        for (int i = 0; i < num.length; i++) {
-            if (!flag[i]) {
-                flag[i] = true;
-                sub.add(num[i]);
-                dfs(num, flag, sub, lists);
-                flag[i] = false;
-                sub.remove(sub.size() - 1);
+    public int dfs(int[] nums, int index, int S, int target) {
+        if (index == nums.length) {
+            if (S == target) {
+                return 1;
+            } else {
+                return 0;
             }
         }
-        return lists;
+        return dfs(nums, index + 1,S - nums[index], target) +
+               dfs(nums, index + 1,S + nums[index], target);
     }
+
 
     public static void main(String[] args) {
         int[] nums = new int[] {1, 1, 1, 1, 1};
