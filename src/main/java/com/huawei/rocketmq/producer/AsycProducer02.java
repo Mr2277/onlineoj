1388787aabe8c409fc07ecb7c87666696efc9257
sun <837544713@qq.com>
2021-03-25 20:02:30 +0800

    add RocketMQ

--- src/main/java/com/huawei/rocketmq/producer/AsycProducer02.java
+++ src/main/java/com/huawei/rocketmq/producer/AsycProducer02.java
@@ -4,27 +4,34 @@ import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.CountDownLatch2;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
 
 public class AsycProducer02 {
     public static void main(String[] args) throws UnsupportedEncodingException, MQClientException, RemotingException, InterruptedException {
         // 实例化消息生产者Producer
-        DefaultMQProducer producer = new DefaultMQProducer("0325");
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
         // 设置NameServer的地址
         producer.setNamesrvAddr("localhost:9876");
         // 启动Producer实例
         producer.start();
         producer.setRetryTimesWhenSendAsyncFailed(0);
-        //producer.createTopic("key0325", "Topic0325", 2);
 
-        for (int i = 0; i < 10; i++) {
+        int messageCount = 10;
+        // 根据消息数量实例化倒计时计算器
+        final CountDownLatch2 countDownLatch = new CountDownLatch2(messageCount);
+        for (int i = 0; i < messageCount; i++) {
             final int index = i;
             // 创建消息，并指定Topic，Tag和消息体
-            Message msg = new Message("Topic0325Asyc","TagAsyc", "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            Message msg = new Message("TopicTest1",
+                    "TagA",
+                    //"OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
             // SendCallback接收异步返回结果的回调
             producer.send(msg, new SendCallback() {
                 @Override
@@ -39,7 +46,10 @@ public class AsycProducer02 {
                 }
             });
         }
+        // 等待5s
+        countDownLatch.await();
         // 如果不再发送消息，关闭Producer实例。
         producer.shutdown();
+
     }
 }
