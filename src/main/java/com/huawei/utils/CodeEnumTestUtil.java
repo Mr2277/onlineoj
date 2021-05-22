590d9621b1075c4d0e79b1a3115d7d5e5fa0b258
sun <837544713@qq.com>
2021-05-22 14:17:51 +0800

    add 工具类

--- src/main/java/com/huawei/utils/CodeEnumTestUtil.java
+++ src/main/java/com/huawei/utils/CodeEnumTestUtil.java
@@ -1,4 +1,7 @@
 package com.huawei.utils;
 
 public class CodeEnumTestUtil {
+    public static <E extends Enum<?>> E codeOf(Class<E> enumClass, int code) {
+        return enumClass.getEnumConstants()[0];
+    }
 }
