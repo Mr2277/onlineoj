d3302302ec6784b11baa33f00add42cf67047ae6
sun <837544713@qq.com>
2021-05-22 08:48:29 +0800

    add test

--- src/main/java/com/huawei/test/TestEnum.java
+++ src/main/java/com/huawei/test/TestEnum.java
@@ -1,9 +1,15 @@
 package com.huawei.test;
 
+import com.huawei.utils.CodeEnumUtil;
+
 public class TestEnum {
     public static void main(String[] args) {
         SeasonEnum seasonEnum = SeasonEnum.SPRING;
         System.out.println(SeasonEnum.data.size());
         System.out.println(SeasonEnum.valueOf("SPRING"));
+
+        ComputerState computerState = CodeEnumUtil.codeOf(ComputerState.class, 255);
+        System.out.println(computerState);
+        System.out.println(computerState);
     }
 }
